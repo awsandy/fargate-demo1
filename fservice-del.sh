51fdf3c88ecccde29cec7fcab9d13957a367d4e3
awsandy <awsandy@amazon.co.uk>
2020-02-20 14:24:44 +0000

    initial

--- fservice-del.sh
+++
@@ -1,6 +0,0 @@
-fargate service scale myapp 0 --region eu-west-1
-fargate service destroy myapp --region eu-west-1
-fargate lb destroy myapp --region eu-west-1
-
-# $ npm install -g http-server   # install dependency
-# $ http-server -p 8000
